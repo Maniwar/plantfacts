aa1c77cd506e414890127fa941a9dd47229e2980
Maniwar <mberenji@gmail.com>
2025-08-24 13:00:41 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,10 +7,3 @@ Author: Maniwar
 # Package metadata
 __version__ = "2.5.1"
 __author__ = "Maniwar"
-
-# We intentionally don't import anything here to avoid circular import issues
-# that occur on Streamlit Cloud. Each module should be imported directly
-# where needed using:
-#   from utils.config import AppConfig
-#   from utils.cache_service import CacheService
-#   etc.
