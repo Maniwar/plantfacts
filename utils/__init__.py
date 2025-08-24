b2e25122e3375641651cd8a427a9321d42661f57
Maniwar <mberenji@gmail.com>
2025-08-24 13:00:31 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,3 @@
-
 """
 Utils Package
 Contains all utility modules for the Plant Facts Explorer application
@@ -6,5 +5,12 @@ Author: Maniwar
 """
 
 # Package metadata
-__version__ = "2.1.0"
+__version__ = "2.5.1"
 __author__ = "Maniwar"
+
+# We intentionally don't import anything here to avoid circular import issues
+# that occur on Streamlit Cloud. Each module should be imported directly
+# where needed using:
+#   from utils.config import AppConfig
+#   from utils.cache_service import CacheService
+#   etc.
